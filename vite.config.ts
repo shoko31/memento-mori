67fce750f6978bc88e09571a6566a3be9becd30e
Elliott Z <elliott.zwertvaegher@3i.ai>
2023-10-26 16:19:14 +0900

    Change base path

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/memento-mori',
   plugins: [
     vue(),
     AutoImport({
